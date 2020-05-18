d646f0a12d61b346a5b9cd2b5f31123dff3d5be6
Литвиненко Юрий <litvinenko-don@ukr.net>
2020-05-18 20:17:10 +0300

    completed task 2

--- js/original.js
+++ js/original.js
@@ -1,34 +1,10 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
 
-/**1 */
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ' ');
-console.log(typeof (numberOfFilms));
-console.log(numberOfFilms);
+// console.log(typeof (numberOfFilms));
+// console.log(numberOfFilms);
 
-/**2 */
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -37,18 +13,39 @@ const personalMovieDB = {
     privat: false
 };
 
-/**3 */
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Еще один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените этот фильм?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB.movies);
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log(personalMovieDB.movies);
+        console.log('record to the object successfully');
+    } else {
+        i--;
+        console.log('record to the object error');
+    }
+}
+
+/*==вариант написания условия с тернарным оператором==*/
+const message = (personalMovieDB.count === '' || personalMovieDB.count < 10) ? 'Просмотрено довольно мало фильмов' :
+    (personalMovieDB.count >= 10 && personalMovieDB.count < 30) ? 'Вы классический зритель' :
+        (personalMovieDB.count >= 30) ? 'Вы киноман' : 'Произошла ошибка';
+
+
+/*==вариант написания условия с if...else==*/
+// let message;
+// if (personalMovieDB.count === '' || personalMovieDB.count < 10) {
+//     message = 'Просмотрено довольно мало фильмов';
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     message = 'Вы классический зритель';
+// } else if (personalMovieDB.count >= 30) {
+//     message = 'Вы киноман';
+// } else {
+//     message = 'Произошла ошибка';
+// }
+
+console.log('Вы посмотрели '  + personalMovieDB.count + ' фильмов, ' + message);
+
 
 const options = {};
-
-const  bool = 'first';
-
-options[bool] = false; 
